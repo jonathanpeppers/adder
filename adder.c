4b72ed9e1d1dcad7041948f86178be4c74f11f88
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-08-19 10:32:37 -0500

    Added a native library

---
+++ adder.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int add (int x, int y)
+{
+   printf("x=%d\n", x);
+   printf("y=%d\n", y);
+   return x + y;
+}
--- adder.cs
+++ adder.cs
@@ -1,8 +1,10 @@
 using System;
+using System.IO;
+using System.Runtime.InteropServices;
 
 class adder
 {
-    public static void Main (string [] args)
+    static void Main (string [] args)
     {
         int x = 0, y = 0;
         if (args.Length > 0) {
@@ -11,6 +13,29 @@ class adder
         if (args.Length > 1) {
             int.TryParse (args [1], out y);
         }
-        Console.WriteLine (x + y);
+        Console.WriteLine (Native.add (x, y));
+    }
+
+    class Native
+    {
+        static Native ()
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
+                string dir = Path.GetDirectoryName (typeof (Native).Assembly.Location);
+                if (Environment.Is64BitProcess) {
+                    Console.WriteLine ("64 bit");
+                    SetDllDirectory (Path.Combine (dir, "x64"));
+                } else {
+                    Console.WriteLine ("32 bit");
+                    SetDllDirectory (Path.Combine (dir, "x86"));
+                }
+            }
+        }
+
+        [DllImport ("kernel32.dll", SetLastError = true)]
+        static extern bool SetDllDirectory (string lpPathName);
+
+        [DllImport ("libadder.dll")]
+        public static extern int add (int x, int y);
     }
 }
